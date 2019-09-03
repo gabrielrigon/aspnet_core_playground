23051218fa25c91411c1cd7568e8c029734c4faa
Gabriel Rigon <gabrielrigon@outlook.com>
2019-09-03 15:46:09 -0300

    feat(controller): implementa create

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -25,7 +25,6 @@ namespace TodoApi.Controllers
       }
     }
 
-    // GET: api/Todo
     [HttpGet]
     public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
     {
@@ -43,6 +42,15 @@ namespace TodoApi.Controllers
       }
 
       return todoItem;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item)
+    {
+      _context.TodoItems.Add(item);
+      await _context.SaveChangesAsync();
+
+      return CreatedAtAction(nameof(GetTodoItem), new { id = item.Id }, item);
     }
   }
 }
